1d51e8c8bc125399ea100fd2ae77c5069f1d4be0
emily <emolito@wgu.edu>
2024-04-18 19:52:48 -0500

    Correctly ran Step 3: Data Testing

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    prices_in_range = data['price'].between(min_price, max_price)
+    assert prices_in_range.all(), "All prices should be between {} and {}".format(min_price, max_price)
+
+
